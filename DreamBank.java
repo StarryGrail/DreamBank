b34bde206c7873c13e164242d9ce66641e8ab218
Asshley Dean Wayne <154043482+StarryGrail@users.noreply.github.com>
2023-12-20 21:02:34 +0800

    Soon to be released

--- DreamBank.java
+++ DreamBank.java
@@ -3,5 +3,6 @@ public class DreamBank{
     public static void main(String[]args){
 
         System.out.println("Coming Soon!");
+        System.out.println("To be created, please stand by!");
     }
 }
